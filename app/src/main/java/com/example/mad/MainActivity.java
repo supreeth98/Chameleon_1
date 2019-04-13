f20adfce382018af0381454d5b50cc28f63fe025
Kavish Hukmani <khukmani@gmail.com>
2019-04-13 19:07:05 +0530

    JSON working, issue reading file and POST half implemented

--- app/src/main/java/com/example/mad/MainActivity.java
+++ app/src/main/java/com/example/mad/MainActivity.java
@@ -13,6 +13,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.util.Log;
+
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -22,6 +24,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
@@ -43,8 +46,8 @@ public class MainActivity extends AppCompatActivity {
         Button Convert=(Button)findViewById(R.id.button2);
         String FileFormatFrom[]={"docx"};
         String FileFormatTo[]={"pdf"};
-        s1.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,FileFormatFrom));
-        s2.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,FileFormatTo));
+        s1.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, FileFormatFrom));
+        s2.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, FileFormatTo));
         Upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,8 +87,9 @@ public class MainActivity extends AppCompatActivity {
         if(uri!=null) {
             File file = new File(uri.getPath());
             String filename = file.getName();
-            Toast.makeText(this,"File"+filename,Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"File "+filename,Toast.LENGTH_SHORT).show();
             int size = (int) file.length();
+            Log.i("Testing", String.valueOf(size));
             byte[] bytes = new byte[size];
             try {
                 BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
@@ -99,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
             String base64EncodedFile = Base64.encodeToString(bytes, Base64.DEFAULT);
+            Log.i("Testing", base64EncodedFile);
             callAPI(filename, base64EncodedFile);
         }
         else {
@@ -108,11 +113,23 @@ public class MainActivity extends AppCompatActivity {
 
     protected void callAPI(String filename, String base64EncodedFile) {
         String URL = "https://v2.convertapi.com/convert/docx/to/pdf?Secret=vQXAaDU4PooP9Ju0";
-        /*String in;
-        JSONObject reader = new JSONObject(in);
-        JSONArray parameters = new JSONArray("Parameters");
-        JSONObject name */ //Make the JSON and send and recieve the response and covert from base64
+        try {
+            JSONObject json = new JSONObject();
+            JSONArray parametersJSON = new JSONArray();
+            JSONObject parametersJSONObject = new JSONObject();
+            parametersJSONObject.put("Name", "File");
+            JSONObject fileValue = new JSONObject();
+            fileValue.put("Name", filename);
+            fileValue.put("Data", base64EncodedFile);
+            parametersJSONObject.put("FileValue", fileValue);
+            parametersJSON.put(parametersJSONObject);
+            json.put("Parameters", parametersJSON);
+            Log.i("Testing", json.toString());//Make the JSON and send and receive the response and covert from base64
 
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
         StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
